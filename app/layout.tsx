f6fc9e0069d072635d3dd5fd74d33e0d283ef038
Paul Dec <paul.decrosse@gmail.com>
2026-01-26 16:14:03 +0700

    new website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'AZERTY.',
+  title: 'AZERTY',
   description: 'Creative studio using new technology',
 }
 
